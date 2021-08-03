11e329a7b50094ed43afd2c1830871aee8c2a3e0
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-03 11:46:42 -0400

    border color

--- src/components/ProductCard/styles.ts
+++ src/components/ProductCard/styles.ts
@@ -1,11 +1,14 @@
 import { StyleSheet } from 'react-native'
 
+const BORDER_COLOR = 'silver'
+
 export default StyleSheet.create({
   container: {
     flex: 1 / 2,
     alignItems: 'center',
     borderRadius: 30,
     borderWidth: 1,
+    borderColor: BORDER_COLOR,
     marginHorizontal: 5,
     padding: 10,
   },
