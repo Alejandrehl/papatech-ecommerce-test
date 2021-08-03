c10b6cfc53035fa2d675bb38e6752c8b78c91982
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-03 14:26:35 -0400

    Save

--- src/redux/reducers/cart.reducer.ts
+++ src/redux/reducers/cart.reducer.ts
@@ -30,7 +30,20 @@ export default (state = initialState, action: any) => {
     case ADD_PRODUCT_TO_CART:
       return {
         ...state,
-        products: [...state.products, action.payload],
+        products: state.products.map((cartProduct: CartProduct) => {
+          if (cartProduct.product.id === action.payload.id) {
+            cartProduct.quantity += 1
+
+            return cartProduct
+          }
+
+          const newProductToAdd: CartProduct = {
+            product: action.payload,
+            quantity: 1,
+          }
+
+          return newProductToAdd
+        }),
         badgeCount: state.badgeCount + 1,
       }
     default:
