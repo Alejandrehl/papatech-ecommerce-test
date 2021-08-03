e9d5cb87991bf98a4793625e41e107f86019d1d1
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-02 22:54:44 -0400

    Eslint config done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'rokket-labs/react-native',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security-node/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['warn'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
