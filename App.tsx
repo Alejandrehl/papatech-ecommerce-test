7d3c97356a26ee061515003079a3a8a68c9c6ba5
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-02 23:12:04 -0400

    Redux config done

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { SafeAreaView, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+
+import store from './src/store'
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>PapaTech Ecommerce</Text>
-      </View>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView>
+        <View>
+          <Text>PapaTech Ecommerce</Text>
+        </View>
+      </SafeAreaView>
+    </Provider>
   )
 }
 
