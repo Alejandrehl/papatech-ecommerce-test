3c2f3fef7e7db7179fdfdcff1ca08d504eac7928
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-03 15:58:27 -0400

    save

--- src/components/MiniCartOverlay/styles.ts
+++ src/components/MiniCartOverlay/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     width: '80%',
-    height: '60%',
+    height: '50%',
     borderRadius: 30,
   },
   content: {
